eed8a2e03e98d3126906040189ef4a50041eff7d
João Pedro <joaopedrowatson@hotmail.com>
2021-04-12 14:49:03 -0300

    definição de classe e metodos

--- economatica.py
+++ economatica.py
@@ -1,57 +1,80 @@
 import json
-import random
-
-"""
-TODO: 
-1. criar uma lista                          [X]
-2. buscar um elemento por “id”              [ ] Retornar posição do objeto com o id escolhido
-3. buscar um elemento por “nome”            [ ] Retornar posição do objeto com o nome escolhido
-4. percorrer todos os elementos da lista    [X]
-"""
-
-jsonObjeto = "Objeto.json"
-acionistaOuAplicacao = "AcionistaOuAplicacao.json"
 
 
-def criarLista(arquivo_json: str, quer_acionista: bool, quer_aplicacao: bool) -> str:
+class ListaPadrao:
+    """Lista Padrao
+    Classe responsável pelas operações principais do programa
+    """
 
-    with open(arquivo_json) as file:  # arquivo_json
-        data = json.load(file)
+    def __init__(self, arquivo_json: str, quer_acionista: bool, quer_aplicacao: bool):
+        """Iniciação da classe/construtor
 
-        if(arquivo_json == 'AcionistaOuAplicacao.json'):
-            if quer_acionista:
-                print([x for x in data if 'acionista' in x])
-            if quer_aplicacao:
-                print([x for x in data if 'aplicacao' in x])
-        else:
-            print(data)
+        Args:
+            arquivo_json (str): arquivo do qual deseja extrair os dados
+            quer_acionista (bool): True para lista de acionista
+            quer_aplicacao (bool): True para lista de aplicação
+        """
+        self.lista = []
+        self.arquivo_json = arquivo_json
+        self.quer_acionista = quer_acionista
+        self.quer_aplicacao = quer_aplicacao
 
-        print("Lista percorrida:")
-        for i in range(len(data)):
-            print(data[i])
+    def criar(self):
+        """Método de criação das listas
+        """
+        with open(self.arquivo_json) as file:  # arquivo_json
+            data = json.load(file)
 
-        print(f'\nTamanho json:\n{len(data)}')
+            if(self.arquivo_json == 'AcionistaOuAplicacao.json'):
+                if self.quer_acionista:
+                    self.lista = [x for x in data if 'acionista' in x]
+                if self.quer_aplicacao:
+                    self.lista = [x for x in data if 'aplicacao' in x]
+            else:
+                print(data)
 
+    def listar_todos(self):
+        """Método responsável por listar todos os itens de uma lista
+        """
+        for i in range(len(self.lista)):
+            print(self.lista[i])
 
-def busca(lista, id_ou_nome, valor):
-    for i in range(len(lista)):
-        if(lista[i][id_ou_nome] == valor):
-            return i
+    def busca_id_nome(self, id_ou_nome: str, valor):
+        """Busca indice do valor escolhido
+
+        Args:
+            id_ou_nome (str): opções de busca
+            valor (str/int): valor que deseja buscar
+
+        Returns:
+            int: indice do  valor procurado
+        """
+        for i in range(len(self.lista)):
+            if(self.lista[i][id_ou_nome] == valor):
+                return i
+
+    def count(self):
+        """Retorna quantidade de itens na lista
+
+        Returns:
+            int: quantidade de intens na lista
+        """
+        return int(len(self.lista))
+
+    def get_item(self, index: int):
+        """Item especifico a partir do index
+
+        Args:
+            index (int): index do objeto na lista
+
+        Returns:
+            dict: Objeto selecionado da lista
+        """
+        return self.lista[index]
 
 
 if __name__ == '__main__':
-    
-    lista = [{'id': 1, 'nome': 'Joao da Silva', 'acionista': True}, {'id': 341, 'nome': 'Antonio de Souza', 'acionista': True}, {
-        'id': 31, 'nome': 'Manoel de Oliveira', 'acionista': True}, {'id': 345341, 'nome': 'Jose dos Santos', 'acionista': True}]
-    
-    busca = busca(lista, "id", 345341)
-    
-    print(busca)
-    
-    #criarLista(arquivo_json=acionistaOuAplicacao,quer_acionista=True, quer_aplicacao=False)
-
-    #sorted(random.choices(range(1000), k=10))
-
-    #resut = busca(lista, 602)
-
-    # print(resut)
+    json_objeto = "Objeto.json"
+    json_acionista_aplicacao = "AcionistaOuAplicacao.json"
+    teste1 = ListaPadrao(json_acionista_aplicacao, True, False)
+    print(teste1)
